513766674e079c6ec9e8cd19b51604d155530f10
dwirx <bangundwir@gmail.com>
2024-03-13 08:21:45 +0000

    update

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -8,6 +8,10 @@ return {
       vim.cmd.colorscheme("catppuccin-mocha")
       vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
       vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+      --new
+      vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "#51B3EC", bold = true })
+      vim.api.nvim_set_hl(0, "LineNr", { fg = "white", bold = true })
+      vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "#FC6736", bold = true })
     end,
   },
 }
